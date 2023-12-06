bfccae50f35fe41afa57a1a6973e7c1fd02878bf
s7lw3k <sylwek08122000@gmail.com>
2023-12-06 18:25:15 +0100

    konfiguracja karmy

---
+++ src/app/karma.conf.js
@@ -0,0 +1,25 @@
+module.exports = function (config) {
+    config.set({
+      basePath: '',
+      frameworks: ['jasmine', '@angular-devkit/build-angular'],
+      plugins: [
+        require('karma-jasmine'),
+        require('karma-chrome-launcher'),
+        require('@angular-devkit/build-angular/plugins/karma')
+      ],
+      client: {
+        clearContext: false
+      },
+      jasmine: {
+        random: false
+      },
+      reporters: ['progress', 'kjhtml'],
+      port: 9876,
+      colors: true,
+      logLevel: config.LOG_INFO,
+      autoWatch: true,
+      browsers: ['Chrome'],
+      singleRun: false,
+      restartOnFileChange: true
+    });
+  };
